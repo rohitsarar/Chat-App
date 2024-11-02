874f884c4575efa5b73a35c60a5dd679160a3265
rohitsarar <rohitgupta280504@gmail.com>
2024-11-01 21:59:11 -0700

    Socket url update

--- Fronted/src/components/context/SocketContext.jsx
+++ Fronted/src/components/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://chat-app-1-9kye.onrender.com", {
         query: { userId: authUser._id },
       });
 
